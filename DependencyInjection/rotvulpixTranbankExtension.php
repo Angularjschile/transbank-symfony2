aa29f0123f35719c2a54722fa03c0f96be85ec67
Javier Godoy <rotvulpix@gmail.com>
2015-03-29 21:47:54 -0300

    Limpieza

--- DependencyInjection/rotvulpixTranbankExtension.php
+++ DependencyInjection/rotvulpixTranbankExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class rotvulpixtransbankExtension extends Extension
+class rotvulpixTranbankExtension extends Extension
 {
     /**
      * {@inheritDoc}
@@ -23,6 +23,5 @@ class rotvulpixtransbankExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
     }
 }
